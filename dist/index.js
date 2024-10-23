40352c2ef6e03c8803f27f55335993d1f070bb16
Estrella <onamfc@gmail.com>
2024-10-23 11:24:35 -0700

    npm cache clean

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,4 @@ const remoteLogger_1 = require("./remoteLogger");
 const devLogger_1 = require("./devLogger");
 // Create a singleton instance of DevLogger
 exports.dev = new devLogger_1.DevLogger();
-exports.remote = new remoteLogger_1.RemoteLogger();
+exports.remote = new remoteLogger_1.RemoteLogger('https://api.yourloggingendpoint.com');
