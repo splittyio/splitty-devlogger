39478ba71dba132e9d3fd6f8025ea7e9d4d329ea
Estrella <onamfc@gmail.com>
2024-10-23 11:25:31 -0700

    rebuild

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { RemoteLogger } from './remoteLogger';
-import { DevLogger } from './devLogger';
-export declare const dev: DevLogger;
-export declare const remote: RemoteLogger;
--- dist/interfaces/ILogger.d.ts
+++
@@ -1,6 +0,0 @@
-export interface ILogger {
-    log(...params: any[]): void;
-    warn(...params: any[]): void;
-    info(...params: any[]): void;
-    error(...params: any[]): void;
-}
